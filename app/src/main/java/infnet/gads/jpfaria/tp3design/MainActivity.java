adfa10edf5d44c33d41906d08e144175201c1df6
Jao Pedro <jpfaria19@hotmail.com>
2018-06-08 18:41:21 -0300

    Implementing the button 'I forgot my password', sending email for recovery

--- app/src/main/java/infnet/gads/jpfaria/tp3design/MainActivity.java
+++ app/src/main/java/infnet/gads/jpfaria/tp3design/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -20,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
 
     Button btnSingup;
     Button btnSubmit;
+    Button btnReset;
 
     EditText user;
     EditText password;
@@ -31,14 +33,41 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        user = findViewById(R.id.fieldUsername);
+        user = findViewById(R.id.fieldUseremail);
         password = findViewById(R.id.fieldPassword);
+        btnReset = findViewById(R.id.btnResetPassword);
 
         firebaseAuth = FirebaseAuth.getInstance();
+
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            // Forget Password
+            public void onClick(View v) {
+                String email = user.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(getApplication(), "Digite seu e-mail", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                firebaseAuth.sendPasswordResetEmail(email)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(MainActivity.this, "Instruções para redefinir sua senha foram enviadas para seu e-mail por favor.", Toast.LENGTH_SHORT).show();
+                                } else {
+                                    Toast.makeText(MainActivity.this, "Falha ao enviar e-mail de redefinição!", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
+
+
     }
 
-    public void alert(String s){
-        Toast.makeText(this,s,Toast.LENGTH_LONG).show();
+    public void alert(String s) {
+        Toast.makeText(this, s, Toast.LENGTH_LONG).show();
     }
 
     public void btnSub(View v) {
@@ -49,12 +78,12 @@ public class MainActivity extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         progressDialog.dismiss();
 
-                        if (task.isSuccessful()){
+                        if (task.isSuccessful()) {
                             Toast.makeText(MainActivity.this, "Login realizado com sucesso", Toast.LENGTH_LONG).show();
                             Intent intent = new Intent(MainActivity.this, LinearActivity.class);
                             intent.putExtra("Email", firebaseAuth.getCurrentUser().getEmail());
                             startActivity(intent);
-                        }else{
+                        } else {
                             Log.e("ERROR", task.getException().toString());
                             Toast.makeText(MainActivity.this, task.getException().getMessage(), Toast.LENGTH_LONG).show();
                         }
@@ -74,9 +103,11 @@ public class MainActivity extends AppCompatActivity {
             alert("Login ou senha incorretos.");
         }*/
 
+
     }
 
-    public void btnSingUp(View v){
+
+    public void btnSingUp(View v) {
         Intent RegisterActivity = new Intent(this, TableActivity.class);
         btnSingup = (Button) findViewById(R.id.btnSingup);
         startActivity(RegisterActivity);
